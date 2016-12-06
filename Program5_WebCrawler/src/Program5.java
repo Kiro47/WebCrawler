ef92b4c707930e1303d251cb39bd36aaf4dbec34
James <jchelm@mtu.edu>
2016-12-06 15:24:41 -0500

    Fix from me being a moron. Refer to previous.

--- Program5_WebCrawler/src/Program5.java
+++ Program5_WebCrawler/src/Program5.java
@@ -1,7 +1,25 @@
 
-public class Program5 {
+public class Program5 extends AbstractWebCrawler{
 
 	public static void main(String[] args) {
 		
+	}
+
+	@Override
+	protected String getWebPage(String domain, int port, String page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void preorderTraversalPrint(WebTreeNode root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public WebTreeNode crawlWeb(String domain, int port, String page, String... searchTerms) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
